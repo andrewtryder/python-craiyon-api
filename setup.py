118f2f720a85e1344272a815956cc1315bae92b7
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-24 12:23:23 -0400

    Update module so it should build.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='craiyonapi',
-    version='0.1',
+    version='0.1.1',
     description='Python wrapper for the Craiyon API',
     author='Andrew Ryder',
     author_email='andrewtryder@gmail.com',
     url='https://github.com/andrewtryder/python-craiyon-api',
-    packages=['python-craiyon-api'],
+    packages=['craiyonapi'],
     install_requires=[
         'requests',
     ],
